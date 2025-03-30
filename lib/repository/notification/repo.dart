cc44b4dc7e461aa78040a5c0332c4c8e2abb537b
alaminpk05 <mdalaminpk05@gmail.com>
2025-03-30 11:46:30 +0600

    configured local notification packege

--- lib/repository/notification/repo.dart
+++ lib/repository/notification/repo.dart
@@ -1,14 +1,3 @@
-
-
 class NotificationServices {
-  // static Future<bool> oneShotAt(
-  //   DateTime time,
-  //   int id,
-  //   Function(int) callback, {
-  //   bool wakeup = true,
-  //   bool rescheduleOnReboot = false,
-  //   bool allowWhileIdle = false,
-  // }) async {
-  //   debugPrint('one shot');
-  // }
+
 }
